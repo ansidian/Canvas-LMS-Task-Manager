20938a5bef40b621086a420db0d8f28764a84049
Andy Su <andysu1551@gmail.com>
2026-01-23 08:37:29 -0800

    feat(03-01): register merge routes under /api/merge
    - Import and mount merge router in routes index
    - Endpoint now accessible at POST /api/merge
    - Protected by requireUser() middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ import eventsRoutes from "./events-routes.js";
 import settingsRoutes from "./settings-routes.js";
 import canvasRoutes from "./canvas-routes.js";
 import canvasGuestRoutes from "./canvas-guest-routes.js";
+import mergeRoutes from "./merge-routes.js";
 
 const router = Router();
 
@@ -11,6 +12,7 @@ router.use("/classes", classesRoutes);
 router.use("/events", eventsRoutes);
 router.use("/canvas", canvasRoutes);
 router.use("/guest/canvas", canvasGuestRoutes);
+router.use("/merge", mergeRoutes);
 router.use("/", settingsRoutes);
 
 export default router;
